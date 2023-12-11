a6639d4d85c0cf8ed317104dbff6f0c12593e0d7
LaneBrantley <122322578+LaneBrantley@users.noreply.github.com>
2023-12-11 05:14:07 -0500

    logout

--- src/Logout/Logout.js
+++ src/Logout/Logout.js
@@ -1,9 +1,17 @@
-import './App.css';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function Logout() {
+const navigate = useNavigate();
+
+  function logoutClick() {
+    localStorage.clear();
+    navigate('/login');
+
+  }
+
   return (
     <div className="App">
-
+      <button onClick={logoutClick}>Logout</button>
     </div>
   );
 }
